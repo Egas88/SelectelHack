2a93cae46bf55f3734c022663a418c6f80fb440a
weinenwollen <tsvetkov.ti@edu.spbstu.ru>
2024-02-23 13:38:27 +0300

    [bugFix] Not working simple code

--- start/start.py
+++ start/start.py
@@ -7,7 +7,7 @@ def handle_start(bot, message):
     itembtn2 = types.InlineKeyboardButton('Логин', callback_data='login')
     itembtn3 = types.InlineKeyboardButton('Помощь', callback_data='login')
     markup.add(itembtn1, itembtn2, itembtn3)
-
+    #
     bot.send_message(message.chat.id, "Привет! Я бот DonorSearch. Для регистрации, пожалуйста, выберите способ "
                                       "отправки данных", reply_markup=markup)
 
