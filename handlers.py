1ae2563f0f09170d3c7ac71b39e23cd7c0ec173f
weinenwollen <tsvetkov.ti@edu.spbstu.ru>
2024-02-23 14:35:32 +0300

    [codeDrop] Bot отдельно

---
+++ handlers.py
@@ -0,0 +1,29 @@
+
+from telebot import types
+from auth_register.register import handle_register
+from auth_register.auth import handle_login
+from start.start import handle_start
+from bot import bot
+
+@bot.message_handler(commands=['login'])
+def login(message):
+    handle_login(bot, message)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Регистрация', commands=['register'])
+def register(message):
+    handle_register(bot, message)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    handle_start(bot, message)
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text == "Регистрация":
+        register(message)
+    elif message.text == "Логин":
+        login(message)
+    else:
+        pass
