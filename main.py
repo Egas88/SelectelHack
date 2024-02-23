8f1441a2d6fbd9f04a056269ddc33ad31c7c255f
egas88 <chereg21@yandex.ru>
2024-02-23 12:48:28 +0300

    Init bot

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from api import *
+import telebot
+import os
+from dotenv import load_dotenv
+from telebot import types
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
 
-if __name__ == '__main__':
-    print(API_ADRESS_NEEDS_ID.format("HELLO"))
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, пошёл нахуй!")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else:
+        bot.send_message(message.from_user.id, "Пошёл нахуй!.")
+
+
+bot.polling(none_stop=True, interval=0)
