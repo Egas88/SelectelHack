ced8c813dcd02bcd0a53b0e1578aafc25e4a0b0b
egas88 <chereg21@yandex.ru>
2024-02-25 03:39:51 +0300

    notifications_update

--- notification_manager/notifications.py
+++ notification_manager/notifications.py
@@ -9,13 +9,12 @@
 # def add_notification_on_donation_plan(chat_id, selected_date, message_text):
 #     notify_time = datetime.strptime(selected_date + " 08:00", "%Y-%m-%d %H:%M")
 #     add_notification(chat_id, message_text, notify_time)
+# 
 
-
-# def check_and_send_notifications():
+# def check_notifications():
 #     notifications = get_notifications()
 #     for notification_id, chat_id, message in notifications:
 #         bot.send_message(chat_id, message)
 #         delete_notification(notification_id)
+#     scheduler.add_job(send_notifications, 'interval', minutes=1)
 
-
-# scheduler.add_job(check_and_send_notifications, 'interval', minutes=1)
